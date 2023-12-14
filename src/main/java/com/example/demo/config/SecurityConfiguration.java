f2cbddc949d96fd1b53ac1948a1e6c764d1c7098
SJ70 <seojae70@gmail.com>
2023-12-15 04:18:49 +0900

    init: Spring Security 설정 초기화

---
+++ src/main/java/com/example/demo/config/SecurityConfiguration.java
@@ -0,0 +1,25 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableMethodSecurity()
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                )
+                .cors(cors -> cors.disable())
+                .csrf(csrf -> csrf.disable())
+                .build();
+    }
+}
