32ea73a815bb341a9f7ca1cc748848fa2d1aa4f5
SJ70 <seojae70@gmail.com>
2023-12-15 18:47:09 +0900

    feat: h2 콘솔 연결

--- src/main/java/com/example/demo/config/SecurityConfiguration.java
+++ src/main/java/com/example/demo/config/SecurityConfiguration.java
@@ -2,12 +2,10 @@ package com.example.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-@EnableMethodSecurity()
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration {
@@ -16,10 +14,13 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/h2-console/**").permitAll()
                         .anyRequest().permitAll()
                 )
-                .cors(cors -> cors.disable())
                 .csrf(csrf -> csrf.disable())
+                .headers((headers -> headers
+                        .frameOptions(frameOptions -> frameOptions.disable()))
+                )
                 .build();
     }
 }
